3d2ed4b44eaee6ad362c63d201e843c019c68e58
Zee <pesquera@mclinica.com>
2017-03-23 14:54:39 +0800

    Fixed bug on willTransition for pageViewController

--- KZSegmentedPageView/KZSegmentedPageView.swift
+++ KZSegmentedPageView/KZSegmentedPageView.swift
@@ -301,7 +301,6 @@ extension KZSegmentedPageView: UIPageViewControllerDelegate {
         if let firstViewController = pendingViewControllers.first,
             let index = segments.indexOf({ $0.viewController == firstViewController }) {
             delegate?.segmentedPageView(self, willUpdatePageIndex: index)
-            segmentedControl.selectedSegmentIndex = index
         }
     }
     
