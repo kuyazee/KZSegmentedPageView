3eff18a45f9bfe3716d6010aba3c8cd524e40943
Zee <pesquera@mclinica.com>
2017-08-26 04:29:35 +0800

    Plased KZSegmentedPageViewDelegate inside KZSegmentedPageView class

--- KZSegmentedPageView/KZSPHelpers.swift
+++ KZSegmentedPageView/KZSPHelpers.swift
@@ -8,40 +8,6 @@
 
 import UIKit
 
-public protocol KZSegmentedPageViewDelegate: class {
-    
-    /**
-     Called when the number of pages is updated.
-     
-     - parameter segmentedPageViewController: the segmentedPageViewController instance
-     - parameter count: the total number of pages.
-     */
-    func segmentedPageView(_ segmentedPageView: KZSegmentedPageView, didUpdatePageCount count: Int)
-    
-    /**
-     Called when the current index is updated.
-     
-     - parameter segmentedPageViewController: the segmentedPageViewController instance
-     - parameter index: the index of the currently visible page.
-     */
-    func segmentedPageView(_ segmentedPageView: KZSegmentedPageView, didUpdateSegmentIndex index: Int)
-    
-    /**
-     Called when the current index will update.
-     
-     - parameter segmentedPageViewController: the segmentedPageViewController instance
-     - parameter index: the index of the next page to be visible.
-     */
-    func segmentedPageView(_ segmentedPageView: KZSegmentedPageView, willUpdatePageIndex index: Int)
-    
-    /**
-     Called when a segment is clicked.
-     
-     - parameter segmentedPageViewController: the segmentedPageViewController instance
-     - parameter index: the index of the segmentedControl that is clicked.
-     */
-    func segmentedPageView(_ segmentedPageView: KZSegmentedPageView, didSelectSegmentAtIndex index: Int)
-}
 
 public struct KZSegment {
     public let title:String
